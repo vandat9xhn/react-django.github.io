950c2f71e1dc470a89aca1cb1c54c21eb37cd600
Nguyen Dat <vandat9xiloveyou@gmail.com>
2021-09-22 10:51:11 +0700

    Fixed FooterFashion

--- static/js/6148.js
+++ static/js/6148.js
@@ -1,2 +1,2 @@
-(self.webpackChunkmy_webpack=self.webpackChunkmy_webpack||[]).push([[6148],{26148:(e,u,t)=>{"use strict";Object.defineProperty(u,"__esModule",{value:!0});var l=n(t(67294));function n(e){return e&&e.__esModule?e:{default:e}}function a(e){return l.default.createElement("div",null)}n(t(45697)),a.propTypes={},u.default=a}}]);
+(self.webpackChunkmy_webpack=self.webpackChunkmy_webpack||[]).push([[6148],{26148:(e,u,a)=>{"use strict";Object.defineProperty(u,"__esModule",{value:!0});var t=r(a(67294));function r(e){return e&&e.__esModule?e:{default:e}}function c(e){return t.default.createElement("div",{className:"bg-primary"})}r(a(45697)),c.propTypes={},u.default=c}}]);
 //# sourceMappingURL=6148.js.map
